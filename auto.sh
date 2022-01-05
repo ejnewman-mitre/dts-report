e825936bd7d76083e91c56e25ff61ba0f2c87609
Eric Newman <ejnewman@mitre.org>
2022-01-05 14:29:39 -0500

    Updates

--- auto.sh
+++ auto.sh
@@ -1,3 +1,3 @@
 #/bin/zsh
-open "https://dts.mitre.org/orgReport.aspx?txtDept=L1A1&amp;showdetail=0"
+open "https://dts.mitre.org/orgReport.aspx?txtDept=L261&amp;showdetail=0"
 safari-ctl -j 'document.getElementsByTagName("HTML")[0].outerHTML' > test2.html
